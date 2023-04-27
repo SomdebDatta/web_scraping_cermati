c2f53460154314a62d6ed83d4169ce61084437e8
SomdebDatta <somdebdatta@gmail.com>
2023-04-28 00:18:37 +0530

    parallelized this b

--- solution2.py
+++ solution2.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import json
 
 import requests
@@ -6,62 +7,65 @@ from bs4 import BeautifulSoup
 from utility.constants import Constants
 from utility.utils import remove_html_tags
 
+
 def main():
+    job_links = []
+    all_jobs = {}
 
     def extract():
-        
         url = Constants.PARENT_URL.value
         r = requests.get(url, Constants.HEADERS.value)
         soup = BeautifulSoup(r.content, Constants.HTML_PARSER.value)
 
-        data = (soup.find_all(Constants.SCRIPT.value, id=Constants.ID_INITIALS.value))
-        return data
+        data = soup.find_all(Constants.SCRIPT.value, id=Constants.ID_INITIALS.value)
 
-    def transform(data):
         clean_data = json.loads(data[0].text)
 
-        job_links = [] 
-        for job_link in clean_data['smartRecruiterResult']['all']['content']:
-            job_links.append(job_link['ref'])
-        
-        output_json = {}
-        for job in job_links:
+        for job_link in clean_data["smartRecruiterResult"]["all"]["content"]:
+            job_links.append(job_link["ref"])
 
-            r = requests.get(job)
-            sample_job = r.json()
-            
-            for item in sample_job['customField']:
-                if item['fieldLabel'] == 'Department':
-                    department = item['valueLabel']
-                elif item['fieldLabel'] == 'Country':
-                    country = item['valueLabel']
+    def transform(job):
+        r = requests.get(job)
+        sample_job = r.json()
 
-            output_job = {
-                "title": sample_job['name'],
-                "location": ','.join([sample_job['location']['city'], country]),
-                "description": [remove_html_tags(sample_job['jobAd']['sections']['jobDescription']['text'])],
-                "qualification": [remove_html_tags(sample_job['jobAd']['sections']['qualifications']['text'])],
-                "posted_by": sample_job['creator']['name']
-            }
+        for item in sample_job["customField"]:
+            if item["fieldLabel"] == "Department":
+                department = item["valueLabel"]
+            elif item["fieldLabel"] == "Country":
+                country = item["valueLabel"]
 
-            if department in output_json.keys():
-                output_json[department].append(output_job)
-            else:
-                output_json[department] = [output_job]
-        return output_json
+        job_output = {
+            "title": sample_job["name"],
+            "location": ",".join([sample_job["location"]["city"], country]),
+            "description": [
+                remove_html_tags(
+                    sample_job["jobAd"]["sections"]["jobDescription"]["text"]
+                )
+            ],
+            "qualification": [
+                remove_html_tags(
+                    sample_job["jobAd"]["sections"]["qualifications"]["text"]
+                )
+            ],
+            "posted_by": sample_job["creator"]["name"],
+        }
+
+        if department in all_jobs.keys():
+            all_jobs[department].append(job_output)
+        else:
+            all_jobs[department] = [job_output]
 
     def load(output_json):
-
-        with open('data.json', 'w', encoding='utf-8') as f:
+        with open("data.json", "w", encoding="utf-8") as f:
             json.dump(output_json, f, ensure_ascii=False, indent=4)
-    
-    data = extract()
 
-    transformed_data = transform(data)
-    for item in transformed_data:
-        print(len(transformed_data[item]))
+    extract()
 
-    load(transformed_data)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(transform, job_links)
+
+    load(all_jobs)
+
 
 if __name__ == "__main__":
     main()
