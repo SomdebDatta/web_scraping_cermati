6c2a2e077cbd849834933830c5e4ffffba3bfa77
SomdebDatta <somdebdatta@gmail.com>
2023-04-27 23:43:00 +0530

    updating constants.py

--- solution2.py
+++ solution2.py
@@ -6,61 +6,62 @@ from bs4 import BeautifulSoup
 from utility.constants import Constants
 from utility.utils import remove_html_tags
 
+def main():
 
-def extract():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-    }
-    url = Constants.PARENT_URL.value
-    r = requests.get(url, headers)
-    # return r.status_code
-    soup = BeautifulSoup(r.content, "html.parser")
-
-    data = (soup.find_all("script", id="initials"))
-    return data
-
-def transform(data):
-    clean_data = json.loads(data[0].text)
-
-    job_links = [] 
-    for job_link in clean_data['smartRecruiterResult']['all']['content']:
-        job_links.append(job_link['ref'])
-    
-    output_json = {}
-    for job in job_links:
-
-        r = requests.get(job)
-        sample_job = r.json()
+    def extract():
         
-        for item in sample_job['customField']:
-            if item['fieldLabel'] == 'Department':
-                department = item['valueLabel']
-            elif item['fieldLabel'] == 'Country':
-                country = item['valueLabel']
+        url = Constants.PARENT_URL.value
+        r = requests.get(url, Constants.HEADERS.value)
+        soup = BeautifulSoup(r.content, Constants.HTML_PARSER.value)
 
-        output_job = {
-            "title": sample_job['name'],
-            "location": ','.join([sample_job['location']['city'], country]),
-            "description": [remove_html_tags(sample_job['jobAd']['sections']['jobDescription']['text'])],
-            "qualification": [remove_html_tags(sample_job['jobAd']['sections']['qualifications']['text'])],
-            "posted_by": sample_job['creator']['name']
-        }
+        data = (soup.find_all(Constants.SCRIPT.value, id=Constants.ID_INITIALS.value))
+        return data
 
-        if department in output_json.keys():
-            output_json[department].append(output_job)
-        else:
-            output_json[department] = [output_job]
-    return output_json
+    def transform(data):
+        clean_data = json.loads(data[0].text)
 
-def load(output_json):
+        job_links = [] 
+        for job_link in clean_data['smartRecruiterResult']['all']['content']:
+            job_links.append(job_link['ref'])
+        
+        output_json = {}
+        for job in job_links:
 
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(output_json, f, ensure_ascii=False, indent=4)
+            r = requests.get(job)
+            sample_job = r.json()
+            
+            for item in sample_job['customField']:
+                if item['fieldLabel'] == 'Department':
+                    department = item['valueLabel']
+                elif item['fieldLabel'] == 'Country':
+                    country = item['valueLabel']
 
-data = extract()
+            output_job = {
+                "title": sample_job['name'],
+                "location": ','.join([sample_job['location']['city'], country]),
+                "description": [remove_html_tags(sample_job['jobAd']['sections']['jobDescription']['text'])],
+                "qualification": [remove_html_tags(sample_job['jobAd']['sections']['qualifications']['text'])],
+                "posted_by": sample_job['creator']['name']
+            }
 
-transformed_data = transform(data)
-for item in transformed_data:
-    print(len(transformed_data[item]))
+            if department in output_json.keys():
+                output_json[department].append(output_job)
+            else:
+                output_json[department] = [output_job]
+        return output_json
 
-result = load(transformed_data)
+    def load(output_json):
+
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(output_json, f, ensure_ascii=False, indent=4)
+    
+    data = extract()
+
+    transformed_data = transform(data)
+    for item in transformed_data:
+        print(len(transformed_data[item]))
+
+    load(transformed_data)
+
+if __name__ == "__main__":
+    main()
--- utility/constants.py
+++ utility/constants.py
@@ -3,3 +3,13 @@ from enum import Enum
 class Constants(Enum):
 
     PARENT_URL = "https://www.cermati.com/karir"
+
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    }
+
+    SCRIPT = "script"
+
+    ID_INITIALS = "initials"
+
+    HTML_PARSER = "html.parser"
